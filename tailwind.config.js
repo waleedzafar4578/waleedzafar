7704ed46b736c3a951f53ba4a28c49ae531dec7b
waleedzafar4578 <waleedzafar4578gmail.com>
2024-07-26 10:39:09 +0500

    truing to resolve error from dev

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      mob: "375px",
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px",
+      laptopl: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
